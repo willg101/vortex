4fcbc1409d02b908ef646b3d7cf0d88feaed0b85
Will Groenendyk <wrg3@calvin.edu>
2017-02-25 01:08:56 -0500

    * Socket Server: Consolidate files

--- socket_server.py
+++ socket_server.py
@@ -8,8 +8,29 @@ import sys
 from twisted.python import log
 from twisted.internet import reactor
 
-from python.PluginManager import PluginManager
+from exceptions import AttributeError
 
+class PluginManager:
+
+    def __init__( self, cwd ):
+        self._plugins = []
+        for sd in os.listdir( cwd + '/modules_enabled' ):
+            plugin_file = cwd + '/modules_enabled/' + sd + '/ss_plugin.py'
+            print "checking for " + plugin_file
+            if os.path.isfile( plugin_file ):
+                execfile( plugin_file, { 'plugin_manager' : self, '__dir__' : cwd } )
+
+    def register( self, plugin ):
+        self._plugins.append( plugin )
+
+    def callHook( self, name, data ):
+        for obj in self._plugins:
+            try:
+                method = getattr( obj, name )
+                if callable( method ):
+                    method( data )
+            except AttributeError as e:
+                pass
 
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
