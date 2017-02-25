5dfb34f09db7d136826b9bc26224922409370968
Will Groenendyk <wrg3@calvin.edu>
2017-02-25 00:56:34 -0500

    * Socket Server: Move xdebug, websocket servers to _core module

---
+++ modules_enabled/_core/ws_plugin.py
@@ -0,0 +1,186 @@
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
+from twisted.internet.protocol  import Factory, Protocol
+
+class Bridge:
+
+    def __init__( self ):
+        self._dbgConnection = None
+        self._wsConnection  = None
+
+    def hasWsConnection( self ):
+        return self._wsConnection is not None
+
+    def setWsConnection( self, conn ):
+        self._wsConnection = conn
+
+    def clearWsConnection( self, conn ):
+        if self.hasWsConnection() and conn is None or self._wsConnection == conn:
+            self._wsConnection = None
+
+    def hasDbgConnection( self ):
+        return self._dbgConnection is not None
+
+    def setDbgConnection( self, conn ):
+        self._dbgConnection = conn
+
+    def clearDbgConnection( self, conn ):
+        if self.hasDbgConnection() and conn is None or self._dbgConnection == conn:
+            self._dbgConnection = None
+
+    def sendToWs( self, data ):
+        if self.hasWsConnection():
+            self._wsConnection.sendMessage( data )
+
+    def sendToDbg( self, data ):
+        if self.hasWsConnection():
+            self._dbgConnection.transport.write( data )
+   
+
+class DbgpServerProtocol(Protocol):
+
+    def __init__( self, bridge, plugin_manager ):
+        self.connected_to_client = False
+        self._bridge = bridge
+        self._pluginManager = plugin_manager
+
+    def connectionMade(self):
+        print("Debug Client connecting:")
+        if self._bridge.hasWsConnection():
+            if self._bridge.hasDbgConnection():
+                print( 'We already have an active debugger client; detaching' )
+                self.transport.loseConnection()
+                return
+            self._bridge.setDbgConnection( self )
+            print("We have a websocket client and a debug client")
+            data = {
+                'connection' : self,
+            }
+            self._pluginManager.callHook( 'onDbgOpen', data )
+            self.connected_to_client = True;
+        else:
+            print("No websocket client, detching...")
+            #self.transport.write( "detach -i 0\0" );
+            self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        if self.connected_to_client:
+            data = {
+                'connection' : self,
+                'reason'     : reason,
+            }
+            self._pluginManager.callHook( 'onDbgClose', data )
+            self._bridge.clearDbgConnection( self )
+            print("Removing callback bridge...")
+            if self._bridge.hasWsConnection():
+                print("Informing websocket client of disconnection");
+                self.sendMessageToWebsocket( '<wsserver status="session_end"></wsserver>' )
+
+    def sendMessageToWebsocket(self, message):
+        if self._bridge.hasWsConnection():
+            message = str( len( message ) ) + '\0' + message + '\0'
+            self._bridge.sendToWs( message )
+
+    def dataReceived(self, data):
+        if self._bridge.hasWsConnection():
+            try:
+                root = ET.fromstring(data.split( "\0" )[1])
+                if root.attrib['fileuri']:
+                    with open( root.attrib['fileuri'].replace( 'file://', '') , 'r' ) as f:
+                        first_line = f.readline();
+                        if first_line.startswith( '<?php /* dpoh: ignore */' ):
+                            print("File is marked to be ignored, deatching...")
+                            self.transport.write( "detach -i 0\0" );
+                            self.transport.loseConnection()
+                            return
+            except Exception as e:
+                pass
+            print data;
+            hook_data = {
+                'connection' : self,
+                'message'    : data,
+                'do_send'    : True,
+                'bridge'     : self._bridge,
+            }
+            self._pluginManager.callHook( 'onDbgMessage', hook_data )
+            if hook_data[ 'do_send' ] and hook_data[ 'message' ] and self._bridge.hasWsConnection():
+                self._bridge.sendToWs( hook_data[ 'message' ] )
+        else:
+            self.transport.write( "detach -i 0\0" );
+            self.transport.loseConnection()
+
+class DbgpServerProtocolFactory(Factory):
+
+    def buildProtocol(self, addr):
+        return DbgpServerProtocol( self.dpoh_bridge, self.dpoh_plugin_manager )
+
+
+class DpohWsProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        self._bridge = self.factory.dpoh_bridge
+        self._pluginManager = self.factory.dpoh_plugin_manager
+        print("Client connecting: {0}".format(request.peer))
+        print(" - Headers: {0}".format(request.headers['cookie']))
+        data = {
+            'request'    : request,
+            'connection' : self,
+        }
+        self._pluginManager.callHook( 'onWsConnect', data )
+
+    def onOpen(self):
+        data = {
+            'bridge'     : self._bridge,
+            'connection' : self,
+        }
+        self._pluginManager.callHook( 'onWsOpen', data )
+        if not self._bridge.hasWsConnection():
+            self._bridge.setWsConnection( self )
+        else:
+            self.transport.loseConnection()
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            print("Text message received: {0}".format(payload.decode('utf8')))
+            data = {
+                'message'    : payload,
+                'connection' : self,
+                'bridge'     : self._bridge,
+                'do_send'    : True,
+            }
+            self._pluginManager.callHook( 'onWsMessage', data )
+            if data[ 'do_send' ] and data[ 'message' ] and self._bridge.hasDbgConnection():
+                self._bridge.sendToDbg( payload + '\0' )
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
+        data = {
+            'connection' : self,
+            'was_clean'  : wasClean,
+            'code'       : code,
+            'reason'     : reason,
+        }
+        self._pluginManager.callHook( 'onWsClose', data )
+        self._bridge.clearWsConnection( self )
+
+class CorePlugin:
+        def __init__( self, plugin_manager ):
+            print( "Init core plugin" )
+            self.plugin_manager = plugin_manager
+
+        def defineConnections( self, data ):
+            bridge = Bridge()
+
+            dbg_factory = DbgpServerProtocolFactory()
+            dbg_factory.dpoh_bridge = bridge
+            dbg_factory.dpoh_plugin_manager = self.plugin_manager
+            data['connections'].append( { 'port' : 9000, 'factory' : dbg_factory } )
+
+            ws_factory = WebSocketServerFactory( u"ws://127.0.0.1:3001/bridge" )
+            ws_factory.protocol = DpohWsProtocol
+            ws_factory.dpoh_bridge = bridge
+            ws_factory.dpoh_plugin_manager = plugin_manager 
+            data['connections'].append( { 'port' : 3001, 'factory' : ws_factory } )
+
+plugin_manager.register( CorePlugin( plugin_manager ) )
--- python_ws_test.py
+++ python_ws_test.py
@@ -8,28 +8,18 @@ import sys
 from twisted.python import log
 from twisted.internet import reactor
 
-from python.DpohWsProtocol import DpohWsProtocol
-from python.DbgpServerProtocol import DbgpServerProtocol, DbgpServerProtocolFactory
-from python.Bridge import Bridge
 from python.PluginManager import PluginManager
 
 
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
-
     plugin_manager = PluginManager( os.path.dirname( os.path.realpath( __file__ ) ) )
-    bridge = Bridge()
-    dbg_factory = DbgpServerProtocolFactory()
-    dbg_factory.dpoh_bridge = bridge
-    dbg_factory.dpoh_plugin_manager = plugin_manager
 
-    ws_factory = WebSocketServerFactory( u"ws://127.0.0.1:3001/bridge" )
-    ws_factory.protocol = DpohWsProtocol
-    ws_factory.dpoh_bridge = bridge
-    ws_factory.dpoh_plugin_manager = plugin_manager
-    # factory.setProtocolOptions(maxConnections=2)
+    plugin_manager.callHook( 'preboot', { 'plugin_manager' : plugin_manager } )
+    plugin_manager.callHook( 'boot',    { 'plugin_manager' : plugin_manager } )
 
-    reactor.listenTCP(9000, dbg_factory)
-    reactor.listenTCP(3001, ws_factory)
+    data = { 'connections' : [] }
+    plugin_manager.callHook( 'defineConnections', data )
+    for conn in data['connections']:
+        reactor.listenTCP( conn['port'], conn['factory'] )
     reactor.run()
-
