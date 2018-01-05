d5b5477de7fe1c500ba0b385cabe4db5d80ad867
Will Groenendyk <wrg3@calvin.edu>
2018-01-05 16:39:44 -0500

    * Drop old/superseded modules
     - DROP _core.old  : Old version of _core module
     - DROP login.old  : Old version of login module
     - DROP theme      : Superseded by theme_2 module
     - DROP autoignore : Superseded by autorun module

--- modules_available/_core.old/templates/status_summary.tpl.php
+++
@@ -1,2 +0,0 @@
-<span class="h-padding"><span class="fa fa-fw fa-sort-amount-asc v-padding"></span> <span class="indicator" id="stack_depth">--</span></span>
-<span class="h-padding"><span class="fa fa-fw fa-pie-chart v-padding"></span> <span class="indicator" id="mem_usage">--</span></span>
