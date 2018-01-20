199919d3d10f72060876231eb0cee60167dc5f25
Will Groenendyk <wrg3@calvin.edu>
2018-01-20 14:39:31 -0500

    - Toolbar: Remove unused code

--- modules_available/toolbar/hooks.php
+++ modules_available/toolbar/hooks.php
@@ -6,10 +6,4 @@ function toolbar_render_preprocess( &$data )
 	{
 		$data[ 'implementations' ][ 'toolbar' ][ 'weight' ] = 10;
 	}
-
-	$reordered = [
-		'toolbar' => $data[ 'implementations' ][ 'toolbar' ],
-	];
-	$reordered = array_merge( $reordered, $data[ 'implementations' ] );
-	$data[ 'implementations' ] = $reordered;
 }
