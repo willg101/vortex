436a0be09dc0ab16de7a2842bfbc3cfe33b7ebc4
Will Groenendyk <wrg3@calvin.edu>
2018-01-24 20:44:52 -0500

    - Templates: Drop unused templates

--- modules_available/code_inspector/templates/debugger_main.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="flex" data-split="horizontal" data-split-id="code_inspector">
-	<div class="editor-cell">
-		<?php $show( 'code_cell' ) ?>
-	</div>
-	<?php $show( 'status_cell' ) ?>
-</div>
