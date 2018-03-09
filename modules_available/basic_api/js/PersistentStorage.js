7d0210dcf0d955847e4e8667d830e0ab55179f3b
Will Groenendyk <wrg3@calvin.edu>
2018-03-09 15:56:59 -0500

    * Storage: Add small libraries for abstracting over localStorage
    The plan here is to create an alternative to PersistentStorage.js that
    stores settings to and loads settings from the server

---
+++ modules_available/basic_api/js/PersistentStorage.js
@@ -0,0 +1,136 @@
+namespace( 'BasicApi' ).PersistentStorage = (function( $ )
+{
+	class PersistentStorageError extends Error {};
+
+	var data_store = {};
+
+	/**
+	 * @param string key
+	 *
+	 * @retval string
+	 */
+	function reduceKeyCollisionProbability( key )
+	{
+		return 'basic_api_persistent_storage_local_storage_' + key;
+	}
+
+	/**
+	 * @param string key
+	 */
+	function get( key )
+	{
+		ls_key = reduceKeyCollisionProbability( key );
+		if ( !data_store[ key ] )
+		{
+			var from_ls = localStorage.getItem( ls_key );
+			if ( !( from_ls === null || from_ls === undefined ) )
+			{
+				try
+				{
+					data_store[ key ] = JSON.parse( from_ls );
+				}
+				catch ( e )
+				{
+					throw new PersistentStorageError( 'Invalid JSON: ' + from_ls );
+				}
+			}
+		}
+		return data_store[ key ];
+	}
+
+	/**
+	 * @param string key
+	 * @param mixed  value
+	 *
+	 * @retval mixed
+	 *	The item actually stored; typically `value`, but may be altered when the
+	 *	'set-persistent-data' is triggered
+	 */
+	function set( key, value )
+	{
+		ls_key = reduceKeyCollisionProbability( key );
+		var data = {
+			key : key,
+			new_value : value,
+			prevent : false,
+			old_value : data_store[ key ],
+		};
+		value = data.new_value;
+		publish( 'set-persistent-data', { data : data } );
+		if ( !data.prevent )
+		{
+			localStorage.setItem( ls_key, JSON.stringify( value ) );
+			data_store[ key ] = value;
+		}
+		return value;
+	};
+
+	/**
+	 * @param string key
+	 *
+	 * @retval bool
+	 *	Indicates if the deletion occurred (true) or was prevented when the 'delete-persistent-data'
+	 *	event was triggered (false)
+	 */
+	function del( key )
+	{
+		ls_key = reduceKeyCollisionProbability( key );
+		var data = {
+			key : key,
+			prevent : false,
+			value : data_store[ key ],
+		};
+		publish( 'delete-persistent-data', { data : data } );
+		if ( !data.prevent )
+		{
+			delete data_store[ key ];
+			localStorage.removeItem( ls_key );
+			return true;
+		}
+		return false;
+	}
+
+	// Unit tests
+	subscribe( 'provide-tests', function()
+	{
+		describe( "BasicApi.PersistentStorage", function()
+		{
+			it( "Basic Usage", function()
+			{
+				localStorage.removeItem( reduceKeyCollisionProbability( 'xxx' ) );
+				expect( BasicApi.PersistentStorage.get( 'xxx' ) ).toBeUndefined();
+
+				BasicApi.PersistentStorage.set( 'xxx', { 'hello' : 'world' } );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx' ) ).toBe( 'object' );
+				expect( BasicApi.PersistentStorage.get( 'xxx' ).hello ).toBe( 'world' );
+				expect( localStorage.getItem( reduceKeyCollisionProbability( 'xxx' ) ) ).toBe( JSON.stringify( { 'hello' : 'world' } ) );
+
+				BasicApi.PersistentStorage.del( 'xxx' );
+				expect( BasicApi.PersistentStorage.get( 'xxx' ) ).toBeUndefined();
+				expect( localStorage.getItem( reduceKeyCollisionProbability( 'xxx' ) ) ).toBeNull();
+
+				localStorage.setItem( reduceKeyCollisionProbability( 'xxx-prepopulated' ), JSON.stringify( { foo : 'bar' } ) );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx-prepopulated' ) ).toBe( 'object' );
+				expect( BasicApi.PersistentStorage.get( 'xxx-prepopulated' ).foo ).toBe( 'bar' );
+				BasicApi.PersistentStorage.del( 'xxx-prepopulated' );
+				expect( BasicApi.PersistentStorage.get( 'xxx-prepopulated' ) ).toBeUndefined();
+
+				localStorage.setItem( reduceKeyCollisionProbability( 'xxx-nested' ), JSON.stringify( { foo : { bar : { baz : 'fuzz' } } } ) );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx-nested' ) ).toBe( 'object' );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx-nested' ).foo ).toBe( 'object' );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx-nested' ).foo.bar ).toBe( 'object' );
+				expect( typeof BasicApi.PersistentStorage.get( 'xxx-nested' ).foo.bar.baz ).toBe( 'string' );
+				expect( BasicApi.PersistentStorage.get( 'xxx-nested' ).foo.bar.baz ).toBe( 'fuzz' );
+			} );
+		} );
+	} );
+
+	return {
+		get : get,
+		set : set,
+		del : del,
+
+		PersistentStorageError : PersistentStorageError,
+	};
+
+}( jQuery ) );
---
+++ modules_available/basic_api/js/Persistor.js
@@ -0,0 +1,96 @@
+namespace( 'BasicApi' ).Persistor = (function( $ )
+{
+	class PersistorError extends Error {}
+	PersistorError.prototype.name = 'Vue.Layout.Persistor.PersistorError';
+
+	var Storage = BasicApi.PersistentStorage;
+
+	function generateLocalStorageItemKey( widget_key, item_key )
+	{
+		return widget_key + '_' + item_key;
+	}
+
+	function Persistor( widget_key )
+	{
+		this.widget_key = widget_key;
+		return new Proxy( this, {
+			get : function( target, item_key )
+			{
+				if ( typeof target[ item_key ] == 'undefined' )
+				{
+					target[ item_key ] = Storage.get( generateLocalStorageItemKey( target.widget_key, item_key ) );
+				}
+				return target[ item_key ];
+			},
+
+			set : function( target, item_key, item_value )
+			{
+				if ( typeof item_value == 'undefined' )
+				{
+					delete this[ item_key ];
+				}
+				else
+				{
+					target[ item_key ] = item_value;
+					Storage.set( generateLocalStorageItemKey( target.widget_key, item_key ), item_value );
+				}
+
+				return item_value;
+			},
+
+			deleteProperty : function( target, item_key )
+			{
+				delete target[ item_key ];
+				Storage.del( generateLocalStorageItemKey( target.widget_key, item_key ) );
+			},
+		} );
+	}
+
+	Persistor.PersistorError = PersistorError;
+
+	// Unit tests
+	subscribe( 'provide-tests', function()
+	{
+		describe( "Vue.Layout.Persistor", function()
+		{
+			it( "Basic Usage", function()
+			{
+				var empty_persistor = new Persistor( 'xxx-empty' );
+				delete empty_persistor.proportion;
+				delete empty_persistor.index;
+
+				expect( empty_persistor.proportion ).toBeUndefined();
+				expect( empty_persistor.index ).toBeUndefined();
+
+				var proportion = Math.random();
+				var index = Math.random();
+				var fillable_persistor = new Persistor( 'xxx-fillable' );
+				fillable_persistor.proportion = proportion;
+				fillable_persistor.index = index;
+
+				expect( fillable_persistor.proportion ).toBe( proportion );
+				expect( fillable_persistor.index ).toBe( index );
+
+				proportion = Math.random();
+				index = Math.random();
+				fillable_persistor.proportion = proportion;
+				fillable_persistor.index = index;
+				expect( fillable_persistor.proportion ).toBe( proportion );
+				expect( fillable_persistor.index ).toBe( index );
+
+				var prepoluated_persistor = new Persistor( 'xxx-fillable' );
+				expect( prepoluated_persistor.proportion ).toBe( proportion );
+				expect( prepoluated_persistor.index ).toBe( index );
+
+				delete fillable_persistor.proportion;
+				delete fillable_persistor.index;
+
+				expect( fillable_persistor.proportion ).toBeUndefined();
+				expect( fillable_persistor.index ).toBeUndefined();
+			} );
+		} );
+	} );
+
+	return Persistor;
+
+}( jQuery ));
