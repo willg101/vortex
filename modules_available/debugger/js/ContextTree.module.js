69cf612a6256b1cb02081cd0ff71d09e928c8436
Will Groenendyk <wrg3@calvin.edu>
2018-08-28 15:15:53 -0400

    * ContextTree: Clean up code

--- modules_available/debugger/js/ContextTree.module.js
+++ modules_available/debugger/js/ContextTree.module.js
@@ -1,12 +1,60 @@
 var $ = jQuery;
 
+/**
+ * @brief
+ *	jQuery plugin to build a jsTree representingthe value of one or more variables
+ *
+ * Samples Usages:
+ *	var data = await BasicApi.Debugger.command( 'property_get' ... );
+ *
+ *	# Basic
+ *	$( '.my-selector' ).vtree( data.parsed.value )
+ *
+ *	# Hide all icons
+ *	$( '.my-selector' ).vtree( data.parsed.value, { pickIcon : () => '' } );
+ *
+ *	# Show only bool values
+ *	$( '.my-selector' ).vtree( data.parsed.value, { preprocess : prop => return prop.type == 'bool' } );
+ *
+ * Supported options:
+ *	- `pickIcon`    : A function that receives the property being added to the tree and returns
+ *	                  string containing the CSS icon class
+ *	- `preprocess`  : A function that receives three values: a jQuery of an element that $.jstree
+ *	                  will be called on, the data for the tree, and the original value passed to the
+ *	                  plugin. This function is called once for each element in the jQuery selection,
+ *	                  and is called immediately before calling $.jstree(). If this returns `false`,
+ *	                  $.jstree is NOT called on the current element (other falsy return values like
+ *	                  `undefined` are ignored)
+ *	- `postprocess` : A function that receives three values: a jQuery of an element that $.jstree
+ *	                  was called on, the data for the tree, and the original value passed to the
+ *	                  plugin. This function is called once for each element in the jQuery selection
+ *	                  that preprocess() did NOT return `false` for. The return value of this
+ *	                  function is ignored.
+ */
 $.fn.vtree = function( ctx, options )
 {
-	options = options || {};
-	$.extend( options, {
+	function pickIcon( property )
+	{
+		switch ( property.type )
+		{
+			case 'Superglobals' : return 'fa-globe';
+			case 'Locals'       : return 'fa-location-arrow';
+			case 'bool'         : return 'fa-toggle-on';
+			case 'null'         : return 'fa-close';
+			case 'string'       : return 'fa-quote-left';
+			case 'object'       : return 'fa-cogs';
+			case 'array'        : return 'fa-th-list';
+			case 'int'          : return 'fa-hashtag';
+			case 'float'        : return 'fa-dot-circle-o';
+			default             : return 'fa-question-circle-o';
+		}
+	}
+
+	options = $.extend( {
 		preprocess  : () => true,
 		postprocess : () => true,
-	} );
+		pickIcon,
+	}, options || {} );
 
 	function buildContextTree( context, is_recursive )
 	{
@@ -18,20 +66,6 @@ $.fn.vtree = function( ctx, options )
 			var cid         = property.cid;
 			var value       = $( '<div>' ).text( property.value || '' ).html();
 
-			var icon = 'fa-question-circle-o';
-			switch ( property.type )
-			{
-				case 'Superglobals' : icon = 'fa-globe';          break;
-				case 'Locals'       : icon = 'fa-location-arrow'; break;
-				case 'bool'         : icon = 'fa-toggle-on';      break;
-				case 'null'         : icon = 'fa-close';          break;
-				case 'string'       : icon = 'fa-quote-left';     break;
-				case 'object'       : icon = 'fa-cogs';           break;
-				case 'array'        : icon = 'fa-th-list';        break;
-				case 'int'          : icon = 'fa-hashtag';        break;
-				case 'float'        : icon = 'fa-dot-circle-o';   break;
-			}
-
 			if ( property.type == 'null' )
 			{
 				value = 'NULL'
@@ -40,6 +74,8 @@ $.fn.vtree = function( ctx, options )
 			{
 				value = Number( value ) ? 'TRUE' : 'FALSE';
 			}
+
+			var icon = options.pickIcon( property );
 
 			var node = {
 				li_attr : {
@@ -122,3 +158,66 @@ $.fn.vtree = function( ctx, options )
 		options.postprocess( this, tree, ctx );
 	} );
 }
+
+subscribe( 'provide-tests', function()
+{
+	describe( "$.vtree", function()
+	{
+		it( "Basic usage", function()
+		{
+			var element = $( '<div>' );
+			expect( element.hasClass( 'jstree' ) ).toBe( false );
+			element.vtree( [] );
+			expect( element.hasClass( 'jstree' ) ).toBe( true );
+		} );
+
+		it( "pickIcon", function()
+		{
+			var element = $( '<div>' );
+			var didCall = false;
+			element.vtree( [ { value : 'hello', type : 'string' } ], { pickIcon : function( p )
+			{
+				expect( p.value ).toBe( 'hello' );
+				didCall = true;
+			} } );
+			expect( didCall ).toBe( true );
+		} );
+
+		it( "preprocess", function()
+		{
+			var element = $( '<div>' );
+			var didCall = false;
+			var ctx = [ { value : 'hello', type : 'string' } ];
+			element.vtree( ctx, { preprocess : function( ...args )
+			{
+				expect( args.length ).toBe( 3 );
+				expect( args[ 0 ] instanceof jQuery ).toBe( true );
+				expect( typeof args[ 1 ] ).toBe( 'function' );
+				expect( args[ 2 ] ).toBe( ctx );
+				didCall = true;
+			} } );
+			expect( didCall ).toBe( true );
+
+			element = $( '<div>' );
+			expect( element.hasClass( 'jstree' ) ).toBe( false );
+			element.vtree( [], { preprocess : () => false } );
+			expect( element.hasClass( 'jstree' ) ).toBe( false );
+		} );
+
+		it( "postprocess", function()
+		{
+			var element = $( '<div>' );
+			var didCall = false;
+			var ctx = [ { value : 'hello', type : 'string' } ];
+			element.vtree( ctx, { preprocess : function( ...args )
+			{
+				expect( args.length ).toBe( 3 );
+				expect( args[ 0 ] instanceof jQuery ).toBe( true );
+				expect( typeof args[ 1 ] ).toBe( 'function' );
+				expect( args[ 2 ] ).toBe( ctx );
+				didCall = true;
+			} } );
+			expect( didCall ).toBe( true );
+		} );
+	} );
+} );
