c98bd0111b87f6d94afedcc8c4850aec96a02ac6
Will Groenendyk <wrg3@calvin.edu>
2018-08-29 11:37:05 -0400

    * File Module: Improve structure

--- modules_available/debugger/js/File.module.js
+++ modules_available/debugger/js/File.module.js
@@ -1,35 +1,52 @@
+export default { basename, dirname, stripScheme, bytesToHumanReadable };
+
 var $ = jQuery;
 
-class File
+var File = {}
+
+function normalize( filename )
 {
-	
+	filename = filename || '';
+	return filename.replace( /([^\/]+)\/+$/, '$1' );
 }
 
 /**
  * @param string filename
  * @retval string
  */
-File.basename = function( filename )
+function basename( filename )
 {
-	filename = filename || '';
-	return filename.replace( /^.*\//, '' );
+	return normalize( filename ).replace( /^.*\//, '' );
 }
 
 /**
  * @param string filename
  * @retval string
  */
-File.stripScheme = function( filename )
+function dirname( filename )
 {
-	filename = filename || '';
-	return filename.replace( /^file:\/\//, '' ).replace( /\/{2,}/, '/' );
+	filename = normalize( filename );
+	if ( !filename.match( /^([A-z]+:\/)?\// ) )
+	{
+		filename = './' + filename;
+	}
+	return filename.replace( /(^|\/+)[^\/]+$/, '/' );
+}
+
+/**
+ * @param string filename
+ * @retval string
+ */
+function stripScheme( filename )
+{
+	return normalize( filename ).replace( /^file:\/\//, '' );
 }
 
 /**
  * @param int size
  * @retval string
  */
-File.bytesToHumanReadable = function( size )
+function bytesToHumanReadable( size )
 {
 	if ( size == 0 )
 	{
@@ -39,4 +56,46 @@ File.bytesToHumanReadable = function( size )
 	return ( size / Math.pow( 1024, i) ).toFixed (2 ) * 1 + ' ' + [ 'B', 'kB', 'MB', 'GB', 'TB' ][ i ];
 }
 
-export default File;
+subscribe( 'provide-tests', function()
+{
+	describe( 'File', function()
+	{
+		it( 'basename', function()
+		{
+			expect( basename( 'abc.html' ) ).toBe( 'abc.html' );
+			expect( basename( '/abc.html/' ) ).toBe( 'abc.html' );
+			expect( basename( '/root/abc.html' ) ).toBe( 'abc.html' );
+			expect( basename( '//abc.html' ) ).toBe( 'abc.html' );
+		} );
+
+		it( 'dirname', function()
+		{
+			expect( dirname( 'abc.html' ) ).toBe( './' );
+			expect( dirname( '/abc.html/' ) ).toBe( '/' );
+			expect( dirname( '/' ) ).toBe( '/' );
+			expect( dirname( '/root/abc.html' ) ).toBe( '/root/' );
+			expect( dirname( 'file:///root/abc.html' ) ).toBe( 'file:///root/' );
+			expect( dirname( '//abc.html' ) ).toBe( '/' );
+		} );
+
+		it( 'stripScheme', function()
+		{
+			expect( stripScheme( 'abc.html' ) ).toBe( 'abc.html' );
+			expect( stripScheme( '/abc.html/' ) ).toBe( '/abc.html' );
+			expect( stripScheme( '/abc.html' ) ).toBe( '/abc.html' );
+			expect( stripScheme( 'file://abc.html' ) ).toBe( 'abc.html' );
+			expect( stripScheme( '/var/file://abc.html' ) ).toBe( '/var/file://abc.html' );
+		} );
+
+		it( 'bytesToHumanReadable', function()
+		{
+			expect( bytesToHumanReadable( 0 ) ).toBe( '0 B' );
+			expect( bytesToHumanReadable( 50 ) ).toBe( '50 B' );
+			expect( bytesToHumanReadable( 1024 ) ).toBe( '1 kB' );
+			expect( bytesToHumanReadable( 1536 ) ).toBe( '1.5 kB' );
+			expect( bytesToHumanReadable( 3 * 1024 * 1024 ) ).toBe( '3 MB' );
+			expect( bytesToHumanReadable( 4 * 1024 * 1024 * 1024 ) ).toBe( '4 GB' );
+			expect( bytesToHumanReadable( 5 * 1024 * 1024 * 1024 * 1024 ) ).toBe( '5 TB' );
+		} );
+	} );
+} );
