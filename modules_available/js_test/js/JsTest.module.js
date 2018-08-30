2d5a7d4786e6435a1ffb65d371b334389bb73a96
Will Groenendyk <wrg3@calvin.edu>
2018-08-30 17:17:26 -0400

    * JsTest: Convert to module

---
+++ modules_available/js_test/js/JsTest.module.js
@@ -0,0 +1,20 @@
+$( function()
+{
+	if ( Dpoh.settings.js_test_mode )
+	{
+		publish( 'provide-tests' );
+	}
+} )
+
+function provideSampleTest()
+{
+	describe( "JS Test", function()
+	{
+		it( "Verify testing mode is enabled", function()
+		{
+			expect( Dpoh.settings.js_test_mode ).toBe( true )
+		} );
+	} );
+}
+
+subscribe( 'provide-tests', provideSampleTest );
