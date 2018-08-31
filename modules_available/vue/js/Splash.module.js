575eea746f18dbd100315c4c712c263500b5d5ef
Will Groenendyk <wrg3@calvin.edu>
2018-08-30 23:06:32 -0400

    * Splash: Clean up code

--- modules_available/vue/js/Splash.module.js
+++ modules_available/vue/js/Splash.module.js
@@ -1,59 +1,23 @@
 import WsClient from '../../debugger/js/WsClient.module.js'
 
-/**
- *
- */
-namespace( 'Vue' ).Splash = (function( $ )
+$( window ).on( 'load', function()
 {
-	function onWindowLoad()
-	{
-		onWindowResized( '*' );
-		$( '.splash-outermost' ).addClass( 'out' )
-			.find( '.full' ).addClass( 'stop' );
+	publish( 'layout-changed' );
+	$( '.splash-outermost' ).addClass( 'out' )
+		.find( '.full' ).addClass( 'stop' );
 
-		var i = 12
-		$( '[data-role=window]' ).each( function()
+	var i = 12
+	$( '[data-role=window]' ).each( function()
+	{
+		setTimeout( function()
 		{
-			setTimeout( function()
-			{
-				$( this ).removeClass( 'not-loaded' );
-			}.bind( this ), i * 50 );
-			i++;
-		} );
-	}
+			$( this ).removeClass( 'not-loaded' );
+		}.bind( this ), i * 50 );
+		i++;
+	} );
+} );
 
-	function onSessionStatusChanged( e )
-	{
-		if ( e.status == 'active' )
-		{
-			Theme.PageTitle.update( 'status', 'active' );
-		}
-		else
-		{
-			Theme.PageTitle.update( 'status', ( WsClient.isConnected() ? 'waiting' : 'disconnected' ) );
-		}
-	}
-
-	function onWindowResized( pane_name )
-	{
-		publish( 'layout-changed', {
-			pane : pane_name,
-		} );
-	};
-
-	function onTransitionEnd( e )
-	{
-		$( e.target ).remove();
-	}
-
-	$( window ).on( 'beforeunload', onUnload );
-	function onUnload()
-	{
-		$( 'body' ).addClass( 'unload' );
-	}
-
-	subscribe( 'session-status-changed',    onSessionStatusChanged );
-	$( window ).on( 'load', onWindowLoad );
-	$( document ).on( 'transitionend', '.splash-outermost', onTransitionEnd );
-
-}( jQuery ));
+$( document ).on( 'transitionend', '.splash-outermost', function( e )
+{
+	$( e.target ).remove();
+} );
