6408cde794ab2a943a6d41b01c3e545e29da9ea8
Will Groenendyk <wrg3@calvin.edu>
2018-09-01 01:00:00 -0400

    * Error Handling: Use Whoops instead of our own built-in exception handler

--- index.php
+++ index.php
@@ -3,59 +3,12 @@
 define( 'DPOH_ROOT', __DIR__ );
 define( 'IS_AJAX_REQUEST', !empty( $_SERVER['HTTP_X_REQUESTED_WITH'] ) );
 
-set_exception_handler( function( $e )
-{
-	$exceptions = [];
-
-	$headers_applied = FALSE;
-	$current = $e;
-	do
-	{
-		$exceptions[] = [
-			'title'     => get_class( $current ),
-			'message'   => $current->getMessage(),
-			'trace'     => $current->getTraceAsString(),
-		];
-
-		if ( $current instanceof HttpException )
-		{
-			$current->applyHeaders();
-			$headers_applied = TRUE;
-		}
-
-	} while( $current = $current->getPrevious() );
-
-	if ( ($c = count( $exceptions ) ) > 1 )
-	{
-		foreach ( $exceptions as $i => $exception )
-		{
-			$i++;
-			$exceptions[ $i - 1 ][ 'title' ] .= " ($i of $c)";
-		}
-	}
-
-	$vars = [
-		'exceptions'   => $exceptions,
-		'n_exceptions' => count( $exceptions ),
-		'base_path'    => is_callable( 'base_path' ) ? base_path() : '',
-	];
-
-	if ( !$headers_applied )
-	{
-		header( 'HTTP/1.0 500 Internal server error' );
-	}
-
-	if ( IS_AJAX_REQUEST )
-	{
-		echo json_encode( $vars );
-	}
-	else
-	{
-		extract( $vars );
-		include 'crash.tpl.php';
-		exit;
-	}
-} );
+require_once 'vendor/autoload.php';
+$whoops = new Whoops\Run;
+$whoops->pushHandler( IS_AJAX_REQUEST
+	? new Whoops\Handler\JsonResponseHandler
+	: new Whoops\Handler\PrettyPageHandler );
+$whoops->register();
 
 require_once 'includes/arrays.php';
 require_once 'includes/autoloader.php';
@@ -69,6 +22,5 @@ require_once 'includes/models.php';
 require_once 'includes/security.php';
 require_once 'includes/stylesheets.php';
 require_once 'includes/templates.php';
-require_once 'vendor/autoload.php';
 
 bootstrap();
