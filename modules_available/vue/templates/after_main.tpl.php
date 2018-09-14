60eead5193d0ba0919f7f881abdf137fbe7569b6
Will Groenendyk <wrg3@calvin.edu>
2018-09-14 13:57:28 -0400

    * Stylesheets: Begin making styles more consistent

--- modules_available/vue/templates/after_main.tpl.php
+++ modules_available/vue/templates/after_main.tpl.php
@@ -2,7 +2,7 @@
 	<div class="modal modal-hidden">
 		<div class="modal-title-bar">
 			<div class="modal-title"></div>
-			<div class="modal-exit"><span class="fa fa-close"></span></div>
+			<div class="modal-exit"><span class="fa fa-times"></span></div>
 		</div>
 		<div class="modal-content">
 		</div>
