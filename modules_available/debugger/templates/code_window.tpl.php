8bda96d4656ddb2a96130f087cd113fc31d66484
Will Groenendyk <wrg3@calvin.edu>
2018-09-14 13:56:52 -0400

    * FontAwesome: Update old icon reference

--- modules_available/debugger/templates/code_window.tpl.php
+++ modules_available/debugger/templates/code_window.tpl.php
@@ -1,7 +1,7 @@
 <div class="flex-wrapper">
 	<div class="file-bar">
 		<input type="text" class="themed-input" placeholder="Enter a filename..." id="file_finder"/>
-		<button class="btn black refresh-file"><i class="fa fa-refresh"></i></span>
+		<button class="btn black refresh-file"><i class="fa fa-sync"></i></span>
 	</div>
 	<div class="relative-container">
 		<pre id="editor" >Start an Xdebug session in order to begin.</pre>
