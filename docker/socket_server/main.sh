d8acc626283af65ab9a633de0bc4b936f691a674
Will Groenendyk <wrg3@calvin.edu>
2018-10-03 12:24:27 -0400

    * Socket Server: Don't attempt to start without a settings file

--- docker/socket_server/main.sh
+++ docker/socket_server/main.sh
@@ -8,10 +8,21 @@ function handle-signal()
 }
 trap handle-signal SIGTERM
 
+cd /var/www/html
+
+# Require a proper settings file to be available before continuing; otherwise, the socket server
+# will immediately terminate
+SETTINGS_FILE=settings-global.json
+POLLING_DELAY=10
+while [[ ! -e "$SETTINGS_FILE" ]]; do
+	echo "'$PWD/$SETTINGS_FILE' does not exist; this file MUST exist before continuing."
+	echo "We'll check again for the file in $POLLING_DELAY seconds..."
+	sleep $POLLING_DELAY
+done
+
 echo "Waiting for 'web' container to come online..."
 /wait-for-it.sh web:80
 
 echo "'web' container is online; starting socket server..."
-cd /var/www/html
 ./vcli socket-server:start &
 wait $!
